5c0c8e5c3a8b5f145e9ba939f2594024c6db775a
Blank67 <hmnshshrm67@gmail.com>
2024-09-03 10:51:07 +0530

    fix meta data part 1

--- src/app/shareProfile/page.js
+++ src/app/shareProfile/page.js
@@ -20,34 +20,47 @@ export default function ShareProfile() {
         cardTitle: "",
         description: "",
     });
+
     const toggleModal = () => {
         setShow((prev) => !prev);
     };
+
     useEffect(() => {
         const fetchMetaData = async (userCode) => {
-            const response = await axios.post(
-                `https://dev.elred.io/noSessionPreviewCardScreenshot?userCode=${userCode}`
-            );
-            console.log(response);
-            if (response.data) {
-                setMetaData(response.data.result[0]);
+            try {
+                const response = await axios.post(
+                    `https://dev.elred.io/noSessionPreviewCardScreenshot?userCode=${userCode}`
+                );
+                if (response.data) {
+                    setMetaData(response.data.result[0]);
+                }
+            } catch (error) {
+                console.error("Failed to fetch meta data:", error);
             }
         };
+
         const query = new URLSearchParams(window.location.search);
         const value = query.get("userCode");
         if (value) fetchMetaData(value);
     }, []);
+
     return (
         <>
             <Head>
                 <title>Profile Data</title>
-                <meta property="og:title" content={metaData.cardTitle} />
-                <meta property="og:image" content={metaData.cardImageURL} />
+                {metaData.cardTitle && (
+                    <meta property="og:title" content={metaData.cardTitle} />
+                )}
+                {metaData.cardImageURL && (
+                    <meta property="og:image" content={metaData.cardImageURL} />
+                )}
                 <meta property="og:url" content={"test.elred.io"} />
-                <meta
-                    property="og:description"
-                    content={metaData.description}
-                />
+                {metaData.description && (
+                    <meta
+                        property="og:description"
+                        content={metaData.description}
+                    />
+                )}
             </Head>
             <Header />
             <UserDetails userData={userData} setUserData={setUserData} />
